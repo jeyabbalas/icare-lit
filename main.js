ed6d954f03a7dff07f9eaf32bf6b3adf9a08a084
Jeya Balaji Balasubramanian <jeyabbalas@gmail.com>
2023-03-05 19:11:11 -0500

    Add usage and privacy notices

--- main.js
+++ main.js
@@ -2,54 +2,72 @@ import './style.css'
 import roseIcon from './rose.svg'
 import githubIcon from './github.svg'
 
+
 const icareLitApp = document.querySelector('#app');
 
 icareLitApp.innerHTML = `
-  <div id="header" class="mx-auto max-w-7xl px-4 py-2 sm:px-6 sm:py-4 lg:px-8 bg-cyan-700 rounded-b-lg">
+  <div id="header" class="mx-auto max-w-7xl px-4 py-2 sm:px-6 sm:py-4 lg:px-8 bg-slate-900 rounded-b-lg">
     <div class="flex items-center justify-between">
       <div class="flex justify-start gap-2">
         <div class="grid place-content-center">
-          <img src="${roseIcon}" class="h-12 w-12 sm:h-16 sm:w-16 gap-2 logo vanilla grayscale" alt="rose icon" />
+          <img src="${roseIcon}" class="h-12 w-12 sm:h-16 sm:w-16 gap-2 logo vanilla" alt="rose logo" />
         </div>
         <div class="min-w-0 flex-1">
           <h2 class="text-2xl font-bold leading-7 text-white sm:text-3xl sm:tracking-tight">iCARE-Lit</h2>
-          <p class="py-2 pr-2 text-sm sm:text-base text-white">An individualized coherent absolute risk estimator of breast cancer</p>
+          <p class="py-2 pr-2 text-sm sm:text-base text-white">Absolute risk estimator for breast cancer</p>
         </div>
       </div>
       
-      <div class="flex md:mt-0 md:ml-4">
+      <div class="flex md:mt-0 md:ml-4 shrink-0">
         <a href="https://github.com/jeyabbalas/icare-lit">
-          <img src="${githubIcon}" class="h-8 w-8 sm:h-12 sm:w-12 flex-no-shrink fill-current" alt="github icon" />
+          <img src="${githubIcon}" class="h-10 w-10 sm:h-12 sm:w-12 fill-current" alt="github logo" />
         </a>
       </div>
     </div>
   </div>
   
-  <div id="info" class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-    
+  <div id="info" class="mx-auto max-w-7xl px-4 py-2 sm:px-6 sm:py-4 lg:px-8">
+    <div class="flex p-4 mb-4 text-sm text-green-800 border border-green-300 rounded-lg bg-green-50" role="alert">
+      <svg aria-hidden="true" class="flex-shrink-0 inline w-5 h-5 mr-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+        <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path>
+      </svg>
+      <span class="sr-only">Data privacy notice</span>
+      <div>
+        <span class="font-medium">Privacy notice</span>: All data is maintained and calculations are performed within your browser. No data is sent to or calculations are performed by any server. The app code is open source and can be inspected by anyone. To view the source code, click on the GitHub logo on the top-right of this page.
+      </div>
+    </div>
+    <div class="flex p-4 mb-4 text-sm text-yellow-800 border border-yellow-300 rounded-lg bg-yellow-50" role="alert">
+      <svg aria-hidden="true" class="flex-shrink-0 inline w-5 h-5 mr-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+        <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path>
+      </svg>
+      <span class="sr-only">App usage notice</span>
+      <div>
+        <span class="font-medium">Usage notice</span>: This app is intended for educational purposes only. It is not a substitute for professional medical advice, diagnosis or treatment. Always seek the advice of your physician or other qualified health provider with any questions you may have regarding a medical condition. Never disregard professional medical advice or delay in seeking it because of something you have read on this app.
+      </div>
+    </div>
   </div>
   
   <div id="form" class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-    <form id="icare-lit-form" class="space-y-8 divide-y divide-gray-200">
-      <div class="space-y-8 divide-y divide-gray-200 sm:space-y-5">
+    <form id="form-icare-lit" class="space-y-8 divide-y divide-gray-200">
+      <div id="form-questionnaire" class="space-y-8 divide-y divide-gray-200 sm:space-y-5">
         <label for="name">Please enter a name or an alias:</label>
         <div>
           <input name="name" type="text" value="Jane Doe">
         </div>
-    </div>
+      </div>
 
-    <div class="pt-6 pb-6">
-        <div class="flex justify-center gap-2 ml-2 mr-2">
-          <button type="submit" class="inline-flex justify-center rounded-md border border-transparent bg-cyan-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-cyan-700 focus:outline-none focus:ring-2 focus:ring-cyan-500 focus:ring-offset-2">Estimate risk</button>
-          <button type="reset" class="rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-cyan-500 focus:ring-offset-2">Reset</button>
+      <div id="form-buttons" class="py-4 sm:py-6">
+        <div class="flex justify-center gap-2">
+          <button type="reset" class="rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-slate-500 focus:ring-offset-2">Reset</button>
+          <button type="submit" class="inline-flex justify-center rounded-md border border-transparent bg-slate-800 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-slate-900 focus:outline-none focus:ring-2 focus:ring-slate-500 focus:ring-offset-2">Estimate risk</button>
         </div>
       </div>
     </form>
   </div>
   
-  <div class="relative">
+  <div id="form-results-divider" class="relative">
     <div class="absolute inset-0 flex items-center" aria-hidden="true">
-      <div class="w-full border-t border-gray-300 ml-2 mr-2"></div>
+      <div class="w-full border-t border-gray-300 sm:mx-8 mx-4"></div>
     </div>
     <div class="relative flex justify-center">
       <span class="bg-white px-2 text-sm text-gray-500">iCARE-Lit results</span>
@@ -57,7 +75,7 @@ icareLitApp.innerHTML = `
   </div>
   
   <div id="results" class="mx-auto max-w-7xl mt-4 px-4 sm:px-6 lg:px-8">
-    <h1>The estimated 5-year risk is: </h1>
+    <h1>The 5-year breast cancer risk projected by iCARE-Lit is: </h1>
   </div>
 `
 
