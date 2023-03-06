8a8c9cb5b40ef6c3fd5318111554126f1cda3bb4
Jeya Balaji Balasubramanian <jeyabbalas@gmail.com>
2023-03-06 02:03:14 -0500

    Style forms with tailwind plugin

---
+++ assets/index-b4e56669.js
@@ -0,0 +1,94 @@
+(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))o(e);new MutationObserver(e=>{for(const s of e)if(s.type==="childList")for(const r of s.addedNodes)r.tagName==="LINK"&&r.rel==="modulepreload"&&o(r)}).observe(document,{childList:!0,subtree:!0});function a(e){const s={};return e.integrity&&(s.integrity=e.integrity),e.referrerPolicy&&(s.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?s.credentials="include":e.crossOrigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function o(e){if(e.ep)return;e.ep=!0;const s=a(e);fetch(e.href,s)}})();const d="/icare-lit/assets/rose-ed9a43c9.svg",n="/icare-lit/assets/github-15128406.svg",l=document.querySelector("#app");l.innerHTML=`
+  <div id="header" class="mx-auto max-w-7xl px-4 py-2 sm:px-6 sm:py-4 lg:px-8 bg-slate-900 rounded-b-lg">
+    <div class="flex items-center justify-between">
+      <div class="flex justify-start gap-2">
+        <div class="flex items-center">
+          <img src="${d}" class="h-12 w-12 sm:h-16 sm:w-16 gap-2 logo vanilla" alt="rose logo" />
+        </div>
+        <div class="min-w-0 flex-1">
+          <h2 class="text-2xl font-bold leading-7 text-white sm:text-3xl sm:tracking-tight">iCARE-Lit</h2>
+          <p class="py-2 pr-2 text-sm sm:text-base text-white">Absolute risk estimator for breast cancer</p>
+        </div>
+      </div>
+      
+      <div class="flex md:mt-0 md:ml-4 shrink-0">
+        <a href="https://github.com/jeyabbalas/icare-lit">
+          <img src="${n}" class="h-10 w-10 sm:h-12 sm:w-12 fill-current" alt="github logo" />
+        </a>
+      </div>
+    </div>
+  </div>
+  
+  <div id="info" class="mx-auto max-w-7xl px-4 py-2 sm:px-6 sm:py-4 lg:px-8">
+    <div id="info-privacy" class="flex p-4 text-sm text-green-800 border border-green-300 rounded-lg bg-green-50" role="alert">
+      <svg aria-hidden="true" class="flex-shrink-0 inline w-5 h-5 mr-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+        <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path>
+      </svg>
+      <span class="sr-only">Data privacy notice</span>
+      <div>
+        <span class="font-medium">Privacy notice</span>: All data is maintained and all calculations are performed within your browser. No data is sent to and no calculations are performed by any server. The app code is open source and can be inspected by anyone. To view the source code, <a class="font-bold underline" href="https://github.com/jeyabbalas/icare-lit">click here</a>.
+      </div>
+    </div>
+    
+    <div id="info-usage" class="flex p-4 mt-2 text-sm text-yellow-800 border border-yellow-300 rounded-lg bg-yellow-50" role="alert">
+      <svg aria-hidden="true" class="flex-shrink-0 inline w-5 h-5 mr-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+        <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path>
+      </svg>
+      <span class="sr-only">App usage notice</span>
+      <div>
+        <span class="font-medium">Usage notice</span>: This app is intended for educational purposes only. It is not a substitute for professional medical advice, diagnosis, or treatment. Always seek the advice of your physician or other qualified health provider with any questions you may have regarding a medical condition. Never disregard professional medical advice or delay in seeking it because of something you have read on this app.
+      </div>
+    </div>
+  </div>
+  
+  <div id="form" class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 border rounded-lg">
+    <form id="form-icare-lit" class="space-y-8 divide-y divide-gray-200">
+      <div id="form-questionnaire" class="space-y-8 divide-y divide-gray-200 sm:space-y-5">
+        <div class="space-y-6 sm:space-y-5 pt-2 sm:pt-4">
+          <div>
+            <h3 class="text-lg leading-6 font-semibold text-gray-900">Questionnaire</h3>
+            <p class="mt-1 max-w-2xl text-sm text-gray-500">Please answer the following questions to the best of your knowledge.</p>
+          </div>
+          
+          <div class="space-y-6 sm:space-y-5">
+            <div class="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:border-t sm:border-gray-200 sm:pt-5">
+               <label for="name" class="block text-sm font-normal text-gray-700 sm:mt-px sm:pt-2">Please enter a name or an alias.</label>
+               <div class="mt-1 sm:col-span-2 sm:mt-0">
+                 <input type="text" name="name" id="name" autocomplete="name" class="block w-full max-w-lg rounded-md border-gray-300 shadow-sm focus:border-slate-500 focus:ring-slate-500 sm:max-w-xs sm:text-sm" value="Jane Doe" required>
+               </div>
+            </div>
+            
+            <div class="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:border-t sm:border-gray-200 sm:pt-5">
+               <label for="age" class="block text-sm font-normal text-gray-700 sm:mt-px sm:pt-2">What is your age?</label>
+               <div class="mt-1 sm:col-span-2 sm:mt-0">
+                 <input type="number" name="age" id="age" min="0" max="100" step="1" class="block w-full max-w-lg rounded-md border-gray-300 shadow-sm focus:border-slate-500 focus:ring-slate-500 sm:max-w-xs sm:text-sm" required>
+               </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div id="form-buttons" class="py-2 sm:py-4">
+        <div class="flex justify-center gap-2">
+          <button type="reset" class="rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-slate-500 focus:ring-offset-2">Reset</button>
+          <button type="submit" class="inline-flex justify-center rounded-md border border-transparent bg-slate-800 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-slate-900 focus:outline-none focus:ring-2 focus:ring-slate-500 focus:ring-offset-2">Estimate risk</button>
+        </div>
+      </div>
+    </form>
+  </div>
+  
+  <div id="form-results-divider" class="pt-4">
+    <div  class="relative">
+      <div class="absolute inset-0 flex items-center" aria-hidden="true">
+        <div class="w-full border-t border-gray-300 mx-4 sm:mx-6 lg:mx-8"></div>
+      </div>
+      <div class="relative flex justify-center">
+        <span class="bg-white px-2 text-sm text-gray-500">iCARE-Lit results</span>
+      </div>
+    </div>
+  </div>
+  
+  <div id="results" class="mx-auto max-w-7xl mt-4 px-4 sm:px-6 lg:px-8">
+    <h1>The 5-year breast cancer risk projected by iCARE-Lit is: </h1>
+  </div>
+`;l.addEventListener("submit",i=>{i.preventDefault();const t=new FormData(i.target),a=Object.fromEntries(t.entries());console.log(a),console.log(a.name)});
