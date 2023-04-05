cbbcb0346c674cbdc45381072bb5493202f4ec2c
Jeya Balaji Balasubramanian <jeyabbalas@gmail.com>
2023-04-05 06:56:17 -0400

    Fix indentation

--- plots/slider.js
+++ plots/slider.js
@@ -1,83 +1,83 @@
-import { dispatch } from 'd3';
+import {dispatch} from 'd3';
 
 export function slider() {
-  let id;
-  let labelText;
-  let min;
-  let max;
-  let step;
-  let value;
-  const listeners = dispatch('change');
+    let id;
+    let labelText;
+    let min;
+    let max;
+    let step;
+    let value;
+    const listeners = dispatch('change');
 
-  const slider = (selection) => {
-    selection
-      .attr('class', 'accent-slate-600 py-2 sm:py-4');
-
-    selection
-      .selectAll('label')
-      .data([null])
-      .join('label')
-      .attr('for', id)
-      .attr('class', 'pr-2')
-      .text(labelText);
-
-    selection
-      .selectAll('input')
-      .data([null])
-      .join('input')
-      .attr('type', 'range')
-      .attr('id', id)
-      .attr('min', min)
-      .attr('max', max)
-      .attr('value', value)
-      .on('change', (event) => {
+    const slider = (selection) => {
         selection
-          .selectAll('output')
-          .data([null])
-          .join('output')
-          .attr('id', id)
-          .text(event.target.value);
+            .attr('class', 'accent-slate-600 py-2 sm:py-4');
 
-        listeners.call('change', null, event.target.value);
-      });
+        selection
+            .selectAll('label')
+            .data([null])
+            .join('label')
+            .attr('for', id)
+            .attr('class', 'pr-2')
+            .text(labelText);
 
-    selection
-      .selectAll('output')
-      .data([null])
-      .join('output')
-      .attr('id', id)
-      .attr('class', 'pl-2 sm:pl-4')
-      .text(value);
-  };
+        selection
+            .selectAll('input')
+            .data([null])
+            .join('input')
+            .attr('type', 'range')
+            .attr('id', id)
+            .attr('min', min)
+            .attr('max', max)
+            .attr('value', value)
+            .on('change', (event) => {
+                selection
+                    .selectAll('output')
+                    .data([null])
+                    .join('output')
+                    .attr('id', id)
+                    .text(event.target.value);
 
-  slider.id = function (_) {
-    return arguments.length ? ((id = _), slider) : id;
-  }
+                listeners.call('change', null, event.target.value);
+            });
 
-  slider.labelText = function (_) {
-    return arguments.length ? ((labelText = _), slider) : labelText;
-  }
+        selection
+            .selectAll('output')
+            .data([null])
+            .join('output')
+            .attr('id', id)
+            .attr('class', 'pl-2 sm:pl-4')
+            .text(value);
+    };
 
-  slider.min = function (_) {
-    return arguments.length ? ((min = +_), slider) : min;
-  }
+    slider.id = function (_) {
+        return arguments.length ? ((id = _), slider) : id;
+    }
 
-  slider.max = function (_) {
-    return arguments.length ? ((max = +_), slider) : max;
-  }
+    slider.labelText = function (_) {
+        return arguments.length ? ((labelText = _), slider) : labelText;
+    }
 
-  slider.step = function (_) {
-    return arguments.length ? ((step = +_), slider) : step;
-  }
+    slider.min = function (_) {
+        return arguments.length ? ((min = +_), slider) : min;
+    }
 
-  slider.value = function (_) {
-    return arguments.length ? ((value = +_), slider) : value;
-  }
+    slider.max = function (_) {
+        return arguments.length ? ((max = +_), slider) : max;
+    }
 
-  slider.on = function () {
-    let value = listeners.on.apply(listeners, arguments);
-    return value === listeners ? slider : value;
-  };
+    slider.step = function (_) {
+        return arguments.length ? ((step = +_), slider) : step;
+    }
 
-  return slider;
+    slider.value = function (_) {
+        return arguments.length ? ((value = +_), slider) : value;
+    }
+
+    slider.on = function () {
+        let value = listeners.on.apply(listeners, arguments);
+        return value === listeners ? slider : value;
+    };
+
+    return slider;
 }
